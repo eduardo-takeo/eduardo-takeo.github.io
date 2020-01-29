d79327317a92ea9b255962cb2427a5d4afb4cfd5
eduardo-takeo <dudu.takeo96@gmail.com>
2020-01-29 18:58:45 -0300

    fix: responsiveness

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 // Collapse top menu
 let navMenu = document.getElementById("js-menu");
 let toggleButton = document.getElementById("js-navbar-toggle");
+let solidMenu = document.getElementById("js-solid-menu")
 
 toggleButton.addEventListener('click', function() {
     if(navMenu.style.display === "none")
@@ -9,9 +10,17 @@ toggleButton.addEventListener('click', function() {
         navMenu.style.display = "none"     
 });
 
+//:TODO
 // toggleButton.addEventListener('click', function() {
 //     if(navMenu.className.match("navbar-toggle"))
 //         navMenu.className = "navbar-toggle-active"
 //     else
 //         navMenu.className = "navbar-toggle"
-// });        
+// });
+
+window.addEventListener("scroll", function() {
+    if(solidMenu.style.background === "rgba(28, 35, 49, 0.2)")
+        solidMenu.style.background = rgba(28, 35, 49, 1)
+    else
+        solidMenu.style.background = rgba(28, 35, 49, 0.2);
+});

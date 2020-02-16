da061d36dd8713ccc81784056d49e60fb72068fb
eduardo-takeo <dudu.takeo96@gmail.com>
2020-02-16 15:47:17 -0300

    added tools section

--- js/main.js
+++ js/main.js
@@ -17,10 +17,3 @@ toggleButton.addEventListener('click', function() {
 //     else
 //         navMenu.className = "navbar-toggle"
 // });
-
-window.addEventListener("scroll", function() {
-    if(solidMenu.style.background === "rgba(28, 35, 49, 0.2)")
-        solidMenu.style.background = rgba(28, 35, 49, 1)
-    else
-        solidMenu.style.background = rgba(28, 35, 49, 0.2);
-});
